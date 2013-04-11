9b03a06ec13803f87eba3fbae0a40d9f60806309
timcog <timcog@gmail.com>
2013-04-11 13:29:51 +0100

    Added functionality and passed JUnit tests as of 11/04/2013

--- Calculator/src/Tests/testScientificCalculator.java
+++ Calculator/src/Tests/testScientificCalculator.java
@@ -4,17 +4,12 @@ import static org.junit.Assert.*;
 import ie.gcd.maths.ScientificCalculator;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
 public class testScientificCalculator {
 
 	public testScientificCalculator() {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
 	}
 
 	@Before
@@ -30,5 +25,25 @@ public class testScientificCalculator {
 		ScientificCalculator sc = new ScientificCalculator();
 		assertEquals("Test add",3, sc.add(2, 1));
 	}
+	
+	@Test
+	public void testSubtract() {
+		ScientificCalculator sc = new ScientificCalculator();
+		assertEquals("Test subtact",1, sc.subtract(2, 1));
+	}
+	
+	@Test
+	public void testMultiply() {
+		ScientificCalculator sc = new ScientificCalculator();
+		assertEquals("Test multiply",20, sc.multiply(4, 5));
+	}
+	
+	@Test
+	public void testDivide() {
+		ScientificCalculator sc = new ScientificCalculator();
+		assertEquals("Test divide",10, sc.divide(20, 2));
+	}
+	
+	
 
 }
--- Calculator/src/ie/gcd/maths/ScientificCalculator.java
+++ Calculator/src/ie/gcd/maths/ScientificCalculator.java
@@ -12,7 +12,19 @@ public class ScientificCalculator {
 	
 	
 	public int add(int x, int y ){
+		return x+y;
+	}
+	
+	public int subtract(int x, int y ){
 		return x-y;
+	}
+	
+	public int multiply(int x, int y ){
+		return x*y;
+	}
+	
+	public int divide(int x, int y ){
+		return x/y;
 	}
 
 	
